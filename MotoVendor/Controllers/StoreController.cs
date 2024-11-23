e5e263d429e50eb53a57907ea0251588871cd654
niedz <niedzmat@gmail.com>
2024-11-23 20:55:54 +0100

    some addoffer changes

--- MotoVendor/Controllers/StoreController.cs
+++ MotoVendor/Controllers/StoreController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLogic.DTO.Offer;
+using BusinessLogic.DTO.VehicleOffer;
+using BusinessLogic.Errors;
+using BusinessLogic.Services;
+using BusinessLogic.Services.Interfaces;
+using DB.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MotoVendor.Controllers
 {
     public class StoreController : Controller
     {
+        
+        IVehicleOfferService _vehicleOfferService;
+        public StoreController(IVehicleOfferService vehicleOfferService)
+        {
+            _vehicleOfferService = vehicleOfferService;
+        }
+
+        [HttpGet]
         public IActionResult AddOffer()
         {
+            //var model = new AddVehicleOfferDTO("", "", "", "", TransmissionType.Manual, VehicleDriveType.FrontWheel, BodyType.Sedan, "", VehicleCondition.Used, );
             return View();
         }
+        [HttpPost]
+        public IActionResult AddOffer(AddVehicleOfferDTO vehicleOffer)
+        {
+            _vehicleOfferService.Add(vehicleOffer);
+            return RedirectToAction("DetailsOffer");
+        }
+
         public IActionResult VehiclesList()
         {
             return View();
