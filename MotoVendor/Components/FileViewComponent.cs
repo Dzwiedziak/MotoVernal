d5ba298bb107c86ac728ff3dee300f7b91467d54
niedz <niedzmat@gmail.com>
2024-11-24 19:28:13 +0100

    file adding

---
+++ MotoVendor/Components/FileViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entities = DB.Entities;
+
+namespace MotoVendor.Components
+{
+    public class FileViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new Entities.File()
+            {
+                Base64 = "",
+                Extension = ""
+            };
+            return View(model);
+        }
+
+        public IViewComponentResult Invoke(Entities.File file)
+        {
+            return View(file);
+        }
+    }
+}
