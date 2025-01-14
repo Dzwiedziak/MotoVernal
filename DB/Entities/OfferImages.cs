406aac49b68ca9af46e9390ae32462551232eb79
niedz <niedzmat@gmail.com>
2025-01-14 21:24:14 +0100

    db changes

---
+++ DB/Entities/OfferImages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Entities
+{
+    public class OfferImages
+    {
+        public int Id { get; set; }
+        public Offer Offer { get; set; }
+        public File Image { get; set; }
+    }
+}
